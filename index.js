deee73393e90fb0088e7ac6ab98bbeb21a45c3ba
saecqwe <105813082+saecqwe@users.noreply.github.com>
2023-05-29 06:08:00 +0500

    Pushing Code using VsCode

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
+
+
+app.get('/chat' , (req , res ) => {
+    res.send("Welcome to Chatting Using SOckets NodeJS")
+})
